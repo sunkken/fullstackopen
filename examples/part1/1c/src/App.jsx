01ef1da5e38f34ff8b45654fec04a8dc1eee5516
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-04 09:22:50 +0300

    prog 1c: page re-rendering

--- examples/part1/1c/src/App.jsx
+++ examples/part1/1c/src/App.jsx
@@ -1,14 +1,9 @@
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-}
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <p>Hello {name}, you are {age} years old</p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
   )
