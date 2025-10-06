260590e8cfdaad33a25df07e0aef4bd66f4421d8
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-06 09:35:30 +0300

    finished 1d examples

--- examples/part1/1d/src/App.jsx
+++ examples/part1/1d/src/App.jsx
@@ -1,27 +1,11 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+const Display = (props) => <div>{props.value}</div>
 
 const Button = (props) => {
-  //console.log('props value is', props)
-  const {onClick, text} = props
   return (
-    <button onClick={onClick}>
-      {text}
+    <button onClick={props.onClick}>
+      {props.text}
     </button>
   )
 }
@@ -36,43 +20,12 @@ const App = () => {
 
   return (
     <div>
-      {value}
-      <button onClick={() => setToValue(1000)}>thousand</button>
-      <button onClick={() => setToValue(0)}>reset</button>
-      <button onClick={() => setToValue(value + 1)}>increment</button>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
-
-
-/*   const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left + updatedRight)
-  }
-
-  return (
-    <div>
-      {left}
-      <Button onClick={handleLeftClick} text="left" />
-      <Button onClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
-      <p>Total clicks: {total}</p>
-    </div>
-  ) */
 }
 
 export default App
