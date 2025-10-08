cede004fb5501fc514eb4a72bb7b40b45fb4fe36
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-08 07:48:35 +0300

    add 1.8: Unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,12 +4,26 @@ const Header = (props) => {
   return <h1>{props.text}</h1>
 }
 
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+
+  return (
+    <div>
+      <div>good {props.good}</div>
+      <div>neutral {props.neutral}</div>
+      <div>bad {props.bad}</div>
+      <div>all {all}</div>
+      <div>average {(props.good - props.bad) / all}</div>
+      <div>positive {props.good / all * 100} %</div>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  let all = good + neutral + bad
 
   return (
     <div>
@@ -18,12 +32,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <Header text="statistics" />
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {(good - bad) / all}</div>
-      <div>positive {good / all * 100} %</div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
