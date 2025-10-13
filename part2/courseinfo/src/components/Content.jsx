926d3835844f5030e75f700d606a595324ca9c44
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-13 09:10:25 +0300

    add 2.2: Course information step 7, 2.3*: Course information step 8

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -6,6 +6,10 @@ const Content = ({content}) => {
       {content.map(part =>
         <Part key={part.id} name={part.name} exercises={part.exercises} />
       )}
+      <h4>total of {content.reduce((sum, part) =>
+        sum + part.exercises, 0)
+        } exercises
+      </h4>
     </div>
   ) 
 }
