ae374558f727acb73850887f2bb16a6f703d8e02
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-13 14:13:18 +0300

    feat finish 2a course information, add 2.5: Separate module step 10

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,5 +1,34 @@
-import Header from './Header'
-import Content from './Content'
+const Header = ({course_name}) => {
+  //console.log(course_name)
+  return (
+  <h2>{course_name}</h2>
+  )
+}
+
+const Part = (part) => {
+  //console.log(part)
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Total = ({content}) => {
+  // console.log(content)
+  return(
+  content.reduce((sum, part) => sum + part.exercises, 0))
+}
+
+const Content = ({content}) => {
+  // console.log(content)
+  return (
+    <div>
+      {content.map(part =>
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+      <h4>total of <Total content={content} /> exercises </h4>
+    </div>
+  ) 
+}
 
 const Course = ({course}) => {
   return (
--- part2/courseinfo/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-const Header = ({course_name}) => {
-  //console.log(course_name)
-  return (
-  <h2>{course_name}</h2>
-  )
-}
-
-export default Header
