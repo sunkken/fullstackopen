5e3d4a891d6c7efc66e93d364bb1b959e3f12dd8
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-14 09:21:55 +0300

    prog 2b Filtering Displayed Elements

--- examples/part2/2b/src/App.jsx
+++ examples/part2/2b/src/App.jsx
@@ -4,10 +4,23 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
 
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1)
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
 
   return (
@@ -19,7 +32,7 @@ const App = (props) => {
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input />
+        <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit">save</button>
       </form>   
     </div>
