a613549d6a149a220f96fc9838ef86931c323eae
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-18 09:29:02 +0300

    add 2.11 The Phonebook Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PersonFilter from './components/PersonFilter'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+
+  useEffect(() => {
+    //console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
+  //console.log('render', persons.length, 'persons')
 
   const handleNameChange = (event) => {setNewName(event.target.value)}
   const handleNumberChange = (event) => {setNewNumber(event.target.value)}
@@ -21,9 +25,7 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    //console.log(persons)
-    //console.log(newName)
-    //Currently trying to hinder all name conflicts.
+    //Currently hindering all name conflicts.
       const nameExists = persons.some(person =>
         person.name.toLowerCase().trim() === newName.toLowerCase().trim())
 
