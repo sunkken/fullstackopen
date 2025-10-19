6f2de173cb1e8a3629eadf455b64c2b1d90bbb19
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-19 03:44:15 +0300

    prog 2d Extracting Communication with the Backend into a Separate Module

--- examples/part2/notes/src/App.jsx
+++ examples/part2/notes/src/App.jsx
@@ -35,6 +35,16 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id === id ? response.data : note))
+    })
+  }
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
@@ -49,7 +59,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+            />
         )}
       </ul>
       <form onSubmit={addNote}>
