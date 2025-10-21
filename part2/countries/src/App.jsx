920516d8f2454fc53d1f89104aff5958c56bcea2
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-21 23:31:36 +0300

    add 2.18* conditional logic for display cases and logic to fetch country details. todo: fix rendering of country details

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -6,7 +6,15 @@ import countryService from './services/countries'
 function App() {
   const [allCountries, setAllCountries] = useState([])
   const [filterName, setFilterName] = useState('')
-  const [country, setCountry] = useState(null)
+  const [countryDetails, setCountryDetails] = useState(null);
+
+  const filteredCountries = allCountries.filter(
+    c => c.toLowerCase()
+    .includes(filterName.toLowerCase())
+  )
+  const country = filteredCountries.length === 1
+    ? filteredCountries[0]
+    : null
 
   useEffect(() => {
     countryService
@@ -15,24 +23,44 @@ function App() {
   }, [])
 
   useEffect(() => {
-    console.log('effect run, country is now', country)
-    if (country) {
-      //code to get country info if search result list is length 1
+    if (!country) {
+      setCountryDetails(null);
+      return;
     }
+
+    countryService
+      .getCountry(country)
+      .then(data => {
+        console.log('Fetched country data:', data);
+        setCountryDetails(data);
+      })
+      .catch(error => {
+        console.error('Error fetching country data:', error);
+      });
   }, [country])
 
   const handleFilterChange = (event) => {setFilterName(event.target.value)}
 
-  const filteredCountries = allCountries.filter(
-    c => c.toLowerCase()
-    .includes(filterName.toLowerCase())
-  )
-
   return (
     <div>
       <CountryFilter filterName={filterName} handleFilterChange={handleFilterChange} />
-      {filteredCountries.map(countryName =>
-        <CountryName key={countryName} name={countryName} />
+
+      {filteredCountries.length > 10 && (
+        <li>Too many matches, specify another filter.</li>
+      )}
+      {filteredCountries.length > 1 && filteredCountries.length <= 10 && (
+        filteredCountries.map(countryName =>
+          <CountryName key={countryName} name={countryName} />
+        )
+      )}
+
+      {filteredCountries.length === 1 && (
+        //code to setCo
+        <h2>{filteredCountries[0]}</h2>
+      )}
+
+      {filteredCountries.length === 0 && (
+        <li>No match found.</li>
       )}
     </div>
   )
