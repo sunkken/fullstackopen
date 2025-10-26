c9bdf9052dbe0f729ba9393549d08c2300764513
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-26 08:14:30 +0200

    prog 3.1a Postman

--- examples/part3/1a_hello/index.js
+++ examples/part3/1a_hello/index.js
@@ -27,6 +27,24 @@ app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
