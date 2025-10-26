1507096152ac9bed8848a0324c7cefe269c1c48b
sunkken <34925100+sunkken@users.noreply.github.com>
2025-10-26 23:12:08 +0200

    prog to exercise 3.1 start

--- examples/part3/1a_hello/index.js
+++ examples/part3/1a_hello/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: "1",
@@ -19,12 +21,12 @@ let notes = [
   }
 ]
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -43,6 +45,33 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001
