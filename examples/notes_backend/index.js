9e0019cb8ffa51235c791011e781bcbb358bc6df
sunkken <34925100+sunkken@users.noreply.github.com>
2025-11-05 10:16:04 +0200

    prog 3d: 3.19* Phonebook database step 7 start

--- examples/notes_backend/index.js
+++ examples/notes_backend/index.js
@@ -17,6 +17,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -63,6 +65,7 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
