c21bb406dbd66227c49ebbe0bc94894ccead05f3
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-01 12:06:37 +0200

    add progress to 4b: Initializing the database before tests; re-add mongo.js and push custom entries to testDB

---
+++ examples/notes_backend/tests/note_api.test.js
@@ -0,0 +1,31 @@
+const { test, after } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, 3)
+})
+
+test('a specific note is within the returned notes', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(e => e.content)
+  assert(contents.includes('TestDB makes things better'))
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
