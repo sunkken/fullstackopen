78c1e1010e31d1f5f1212125d6f9ed0570b5f06f
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-02 11:04:28 +0200

    prog to 4b: Running tests one by one

--- examples/notes_backend/tests/note_api.test.js
+++ examples/notes_backend/tests/note_api.test.js
@@ -1,10 +1,24 @@
-const { test, after } = require('node:test')
 const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Note = require('../models/note')
 
 const api = supertest(app)
+
+const initialNotes = [
+  { content: 'HTML is easy', important: false },
+  { content: 'Browser can execute only JavaScript', important: true },
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
 
 test('notes are returned as json', async () => {
   await api
@@ -16,14 +30,14 @@ test('notes are returned as json', async () => {
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, 3)
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
 
   const contents = response.body.map(e => e.content)
-  assert(contents.includes('TestDB makes things better'))
+  assert(contents.includes('HTML is easy'))
 })
 
 after(async () => {
