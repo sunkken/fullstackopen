548de4ed4d2f1ae84e4cf5580d74537629b52914
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-06 12:00:21 +0200

    downgrade supertest version to same as notesApp example; add 4.8: Blog List Tests, step 1

---
+++ part4/bloglist/bloglist_backend/tests/blog_api.test.js
@@ -0,0 +1,33 @@
+const assert = require('node:assert')
+const { test, after, beforeEach, describe } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+describe('when there are initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.listWithManyBlogs)
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.listWithManyBlogs.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
