800d894b8b5f23dbda5e9e80999ee1d64e75772a
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-13 13:26:20 +0200

    add 4.17: Blog List Expansion, step 5

--- part4/bloglist/bloglist_backend/controllers/blogs.js
+++ part4/bloglist/bloglist_backend/controllers/blogs.js
@@ -1,13 +1,27 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+  .find({}).populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
+
+  // If no userId provided, find the first user in the database
+  if (!user) {
+    user = await User.findOne({})
+  }
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
 
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'title or url missing' })
@@ -18,9 +32,13 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
