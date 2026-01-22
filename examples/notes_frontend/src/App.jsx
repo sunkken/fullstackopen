4940e88266d057a8cea08ddd54cae99cd4943b8d
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-22 15:56:57 +0200

    add logic to the login form

--- examples/notes_frontend/src/App.jsx
+++ examples/notes_frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -59,9 +61,20 @@ const App = () => {
       })
   }
 
-  const handleLogin = (event) => {
+  const handleLogin = async event => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
   const handleNoteChange = (event) => {
