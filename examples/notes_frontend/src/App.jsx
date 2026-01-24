e95eb7845620e55986e7b709f0ba4e9420f16fc1
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-24 11:57:31 +0200

    add token to creating new notes

--- examples/notes_frontend/src/App.jsx
+++ examples/notes_frontend/src/App.jsx
@@ -68,6 +68,7 @@ const App = () => {
 
     try {
       const user = await loginService.login({ username, password })
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
