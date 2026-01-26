e9c472e24355fa7c398db200e01c1a64d3522d79
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-26 13:32:20 +0200

    add 5.1: Blog List Frontend, step 1

--- part4/bloglist/bloglist_frontend/src/App.jsx
+++ part4/bloglist/bloglist_frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)  
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +18,47 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('Invalid Username or Password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <Notification message={errorMessage} />
+
+      {!user && (
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+        />
       )}
+
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      )}
+
+
     </div>
   )
 }
