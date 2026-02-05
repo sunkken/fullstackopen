7dbb60bf1855cff4621fa36ecd971db6554657db
sunkken <34925100+sunkken@users.noreply.github.com>
2026-02-05 08:44:54 +0200

    add 5.7: Blog List Frontend, step 7

--- bloglist/bloglist_frontend/src/components/Blog.jsx
+++ bloglist/bloglist_frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div className="blog">
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visible && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
